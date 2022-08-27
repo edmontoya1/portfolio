db0f7dcc70cad076de2d7503839756aa384cd286
Eduardo Montoya <montoyaeduardo2015@gmail.com>
2022-08-26 19:45:44 -0500

    [Fix]: changed social icons

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import { BsTwitter, BsInstagram } from "react-icons/bs";
-import { FaFacebook } from "react-icons/fa";
+import { BsLinkedin, BsInstagram } from "react-icons/bs";
 
 const SocialMedia = () => {
   return (
     <div className="app__social">
       <div>
-        <BsTwitter />
+        <a href="www.linkedin.com/in/eduardo-montoya-b44390217">
+          <BsLinkedin />
+        </a>
       </div>
       <div>
-        <FaFacebook />
-      </div>
-      <div>
-        <BsInstagram />
+        <a href="https://www.instagram.com/ed.montoya_/">
+          <BsInstagram />
+        </a>
       </div>
     </div>
   );
